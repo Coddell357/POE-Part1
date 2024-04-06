669e8d0fa9e547d8dece44bb2d124d3422182fa1
Coddell357 <ST10450661@vcconnect.edu.za>
2024-04-06 21:58:48 +0200

    Declared variables and message output

--- src/main/java/com/mycompany/poe/part1/POEPart1.java
+++ src/main/java/com/mycompany/poe/part1/POEPart1.java
@@ -8,9 +8,29 @@ package com.mycompany.poe.part1;
  *
  * @author lab_services_student
  */
+import javax.swing.JOptionPane;
+
 public class POEPart1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String username;
+        String password;
+        String firstname;
+        String lastname;
+        
+        username = JOptionPane.showInputDialog(null, "Enter your username");
+        
+        password = JOptionPane.showInputDialog(null, "Enter your password");
+        
+        firstname = JOptionPane.showInputDialog(null, "Enter your firstname");
+        
+        lastname = JOptionPane.showInputDialog(null, "Enter your lastname");
+        
+        JOptionPane.showMessageDialog(null, "Welcome " + firstname + lastname + 
+        " it is great to see you again.");
+        
+        JOptionPane.showMessageDialog(null, "USername or password incorrect, " +
+        "please try again.");
+        
     }
 }
