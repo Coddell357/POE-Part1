78fdd6d74bad7f7a5af0ad373726be530c9e64e1
Coddell357 <ST10450661@vcconnect.edu.za>
2024-04-08 22:05:09 +0200

    Added Conditions to methods

--- src/main/java/com/mycompany/poe/part1/Login.java
+++ src/main/java/com/mycompany/poe/part1/Login.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.poe.part1;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author lab_services_student
@@ -11,27 +13,46 @@ package com.mycompany.poe.part1;
 public class Login {
     
     public static boolean CheckUsername(String username) {
-        return username.length()<=5; //incomplete
+        
+        return username.length()<=5 && username.contains("_"); 
+        
+        if ( username != null && CheckUsername(username) == true) {
+           JOptionPane.showMessageDialog(null, "Username succesfully captured");
+        }else
+            JOptionPane.showMessageDialog(null, "Username is not correctly formatted,please ensure that your username contains an underscore and is no more than 5 characters in length.");
     }
     
     public static boolean CheckPasswordComplexity(String password) {
-        return password.length()>=8 && password. //incomplete
-      
-    if (password.length() >= 8) {
         
+        return password.length()>=8 && password.matches("a-z") && password.matches("A-Z") && password.matches("@#$%^*()-&+=");
+      
+    if ( password != null && CheckPasswordComplexity(password) == true) { //(password.length()>= 8 && password.matches("a-z") && password.matches("A-Z") && password.contains("_") && password.matches("@#$%^*()-&+=")) {
+       JOptionPane.showMessageDialog(null, "Password succesfully captured");
     }else {
-        JOptionPane.showMessageDialog(null, " wrong")
+        JOptionPane.showMessageDialog(null, "Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a number and a special ");
     }
+    }
+  
+    public string registerUser() {
+        
+    }
+    public static boolean LoginUser() {
+        
     }
     
-    public static boolean LoginUser() {
+    public static boolean returnLogicStatus() { //string returnLoginStatus ??
+        
+        if(CheckUsername(username) && CheckPAsswordComplexity(password) == true ) {
+            JOptionPane.showMessageDialog(null, "Welcome " + firstname + lastname + " it is great to see you again.");
+        }else {
+            JOptionPane.showMessageDialog(null, "Username or password incorrect, please try again.");
+        }
+            
         
     }
 
     
     
-    
-    
-            
+                
     
 }
--- src/main/java/com/mycompany/poe/part1/POEPart1.java
+++ src/main/java/com/mycompany/poe/part1/POEPart1.java
@@ -13,23 +13,27 @@ import javax.swing.JOptionPane;
 public class POEPart1 {
 
     public static void main(String[] args) {
-        String username;
-        String password;
+        String username = "";
+        String password = "";
         String firstname;
         String lastname;
         
         username = JOptionPane.showInputDialog(null, "Enter your username");
+        CheckUsername(username);
         
         password = JOptionPane.showInputDialog(null, "Enter your password");
+        CheckPasswordComplexity(password);
+        
         
         firstname = JOptionPane.showInputDialog(null, "Enter your firstname");
         
         lastname = JOptionPane.showInputDialog(null, "Enter your lastname");
         
+        
         JOptionPane.showMessageDialog(null, "Welcome " + firstname + lastname + 
         " it is great to see you again.");
         
-        JOptionPane.showMessageDialog(null, "USername or password incorrect, " +
+        JOptionPane.showMessageDialog(null, "Username or password incorrect, " +
         "please try again.");
         
     }
