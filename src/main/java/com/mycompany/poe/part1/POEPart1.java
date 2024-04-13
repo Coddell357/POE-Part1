0e68f2dbdd928d55ccb58eef64e83d5caaa7e8d1
Coddell357 <ST10450661@vcconnect.edu.za>
2024-04-13 17:23:48 +0200

    Added References

--- src/main/java/com/mycompany/poe/part1/POEPart1.java
+++ src/main/java/com/mycompany/poe/part1/POEPart1.java
@@ -106,3 +106,20 @@ public class POEPart1 {
         
     }
 }
+
+
+/*
+Anonymous. 2021. Boolean Keyword in Java. (Version 1.0-SNAPSHOT) [Source code]. https://www.javatpoint.com/boolean-keyword-in-java (Accessed 1 April 2024)
+Anonymous. 2023. Java Stringformat() Method with Examples. (Version 1.0-SNAPSHOT) [Source code]. https://www.geeksforgeeks.org/java-string-format-method-with-examples/ (Accessed 1 April 2024)
+Baeldung  Michal,A. Java Check a String for Lowercase/Uppercase Letter, Special Character and Digit. (Version 1.0-SNAPSHOT) [Source code]. https://www.baeldung.com/java-lowercase-uppercase-special-character-digit-regex (Accessed 11 April 2024)
+Farrell, J. 2018. Java Programming
+Gravelle, R. 2023. Java WHILE and DO WHILE Loops. (Version 1.0-SNAPSHOT) [Source code]. https://www.developer.com/java/java-while-loops/ (Accessed 10 April 2024)
+IIEVC School of Computer Science.2022. Basic Unit Testing example. (Version 1.0-SNAPSHOT) [Source Code]. https://youtu.be/2UxaA00H3og?si=Pb5TxsUsqDd7Ev5m (Accessed 7 April 2024)
+John. 2021. Equals() vs == in Java-The Real Difference.(Version 1.0-SNAPSHOT) [Source code]. https://youtu.be/AoUVdLWFQw?si=tAhlCgHOCZ1m2rxH (Accessed 9 April 2024)
+McKenzie, C. 2022. User Input with a Java JOptionPane example. (Version 1.0-SNAPSHOT) [Source code]. https://www.theserverside.com/blog/Coffee-Talk-Java-News-Stories-and-Opinions/Java-user-input-with-a-Swing-JOptionPane-example (Accessed 7 April 2024)
+Pankaj. 2022. Java Access Modifiers. (Version 1.0-SNAPSHOT) [Source code]. https://www.digitalocean.com/community/tutorials/java-access-modifiers (Accessed 11 April 2024)
+Patidar, R. 2024. How to Validate Input in Java. (Version 1.0-SNAPSHOT) [Source code]. https://www.delfstack.com/howto/java/java-input-validation/ (Accessed 11 April 2024)
+Ramgulia, G. 2023. Java.equals() Method: A Detailed Tutorial. (Version 1.0-SNAPSHOT) [Source code]. https://ioflood.com/blog/dot-equals-method-java/ (Accessed 9 April)
+Shrivasta, A. 2023. Unit Testing in Java with JUnit. (Version 1.0-SNAPSHOT) [Source code]. https://www.browserstack.com/guide/unit-testing-java (Accessed 11 April 2024)
+*/
+
