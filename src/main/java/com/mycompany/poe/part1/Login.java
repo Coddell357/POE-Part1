ce5203b3bf75aaaf51a016629742bbd463f88439
Coddell357 <ST10450661@vcconnect.edu.za>
2024-04-14 02:25:37 +0200

    Refreshed TestFeatures

--- src/main/java/com/mycompany/poe/part1/Login.java
+++ src/main/java/com/mycompany/poe/part1/Login.java
@@ -11,22 +11,29 @@ package com.mycompany.poe.part1;
  * @author lab_services_student
  */
 public class Login {
+    //making my String variables public so i can use them anywhere without restrictions
     public String username;
     public String password;
     
     public boolean CheckUsername(String username) { 
-        this.username = username;
+        this.username = username; //instance variable = parameter passed to constructor
+        //username input from user will be checked to have less than or 5 characters including an underscore
         if ( username != null && username.length()<=5 && username.contains("_") ) {          
           return true;
-        }else{
+        }else {
            return false; 
         }
         
     }
     
     public boolean CheckPasswordComplexity(String password) {  
-        this.password = password;
-        if ( password != null && password.length()>=8 && password.matches(".*[a-z]*.") && password.matches(".*[0-9]*.") && password.matches(".*[A-Z]*.") && password.matches(".*[!@#$%^&*().*]") ) { 
+        this.password = password; //instance variable = parameter passed to constructor
+        /* Baeldung,M.A(2024)
+           Provides us with proper lengths and matches for our passwird fro  the user to contain thus checking the String password
+           password input from user will be required to have an uppercase & lowercase letter, digit,special character and at least 8 characters
+        */
+        if ( password != null && password.length()>=8 && password.matches(".*[a-z]*.") && password.matches(".*[0-9]*.") 
+            && password.matches(".*[A-Z]*.") && password.matches(".*[!@#$%^&*().*]") ) { 
            return true;
         }else {
            return false; 
@@ -34,14 +41,14 @@ public class Login {
     }
   
     public String registerUser() { 
-        
+        //Used 3 if statements to validate username, password and register the user if both are correctly formatted
         if ( !CheckUsername(username) ) {
-            return "Username is incorrect" ;
+            return "Username is not correctly formatted, please ensure that your username contains"
+                    + " an underscore and is no more than 5 characters in length." ;
         }
-            
-        
         if ( !CheckPasswordComplexity(password) ) {
-            return "Password is incorrect";
+            return "Password is not correctly formatted, please ensure that the passowrd contains at least"
+                    + " 8 characters, a capital letter, a number and a special character.";
         } 
         if (CheckUsername(username) && CheckPasswordComplexity(password)) {
              return "You are now registered";
@@ -50,7 +57,12 @@ public class Login {
     }
     
     public boolean LoginUser(String username, String password) { 
-        if ( this.password.equals(password) && this.username.equals(username)) {
+        
+        /*Ramgulia,G(2023)
+          The equals() method checks if String variables are equal
+          The password and username the user registers with should be the same as when they want to login
+        */
+        if ( this.password.equals(password) && this.username.equals(username)) { //instance variable = parameter passed to constructor
             return true;
         }else {
             return false;
@@ -59,8 +71,11 @@ public class Login {
     }
     
     public String returnLoginStatus() { 
-        
-        
+        /*John(2021)
+          The double equals checks if input eneytred is excatly the same as needed
+          Validating if the users input is the same so they can be registed to output a successfull login message 
+          which is compared to true in LoginUser()
+        */
         if( LoginUser(username,password) == true ) {
            return "Successfull login" ; 
         }else {
--- src/main/java/com/mycompany/poe/part1/POEPart1.java
+++ src/main/java/com/mycompany/poe/part1/POEPart1.java
@@ -12,51 +12,85 @@ import javax.swing.JOptionPane;
 
 public class POEPart1 {
 
+    /*Pankaj(2022)
+      Access Modifiers provide access control if they can be used by other classes or not according to their keyword
+      Public Keyword means it can be used in any class, it is a global vafriable
+    */
     public static void main(String[] args) {
+        //Declaring String variables that will be used in the main class
         String username;
         String password;
         String firstname;
         String lastname;
         
-        Login lg = new Login();
+        Login lg = new Login(); // creating an instance in order to use our methods from Login class
         
-        //prompt user for firstname input
+        /* McKenzie,C(2022)
+           The input dialog field for JOptionPane shows a prompt that allows user to enter required information
+           prompt user for firstname input
+        */
         firstname = JOptionPane.showInputDialog(null, "Enter your firstname");
         
-        //prompt useer for input of firstname
+        //prompt useer for input of lastname
         lastname = JOptionPane.showInputDialog(null, "Enter your lastname");
         
          //prompted user for username input
         username = JOptionPane.showInputDialog(null, "Enter your username"); 
-        lg.CheckUsername(username);
+        lg.CheckUsername(username); //calling a method from Login class
         
+        /* Farrel,J(2018)
+           The if/if else statements allows the program to make a decision given certain requirements and displays given messages from programmer
+           if else statements checks if the username is correctly formatted if not it will display appropriate message
+        */
         if( lg.CheckUsername(username)== true) {
+            
+            /* Farrel,J(2018)
+               The Message Dialog field of JOptionPane displays a message to the user
+               The user will get a message according to if their username meets the username requirements or not
+            */
             JOptionPane.showMessageDialog(null,"Username successfully captured");
         }else {
-        JOptionPane.showMessageDialog(null, "Username is not correctly formatted, please ensure that your username contains an underscore and is no more than 5 characters in length.");
+        JOptionPane.showMessageDialog(null, "Username is not correctly formatted, please ensure that your username contains an"
+          + " underscore and is no more than 5 characters in length.");
         }
         
         
         //prompted user for password input
         password = JOptionPane.showInputDialog(null, "Enter your password");
-        lg.CheckPasswordComplexity(password);
+        lg.CheckPasswordComplexity(password); //calling a method from Login class
         
+        //Checking if password input from user is formatted correctly or not thus a messahge will be displayed
         if(lg.CheckPasswordComplexity(password) == true) {
         JOptionPane.showMessageDialog(null, "Password successfully captured");
         }else {
-        JOptionPane.showMessageDialog(null, "Password is not correctly formatted, please ensure that the passowrd contains at least 8 characters, a capital letter, a number and a special character.");
+        JOptionPane.showMessageDialog(null, "Password is not correctly formatted, please ensure that the passowrd contains at least"
+         + " 8 characters, a capital letter, a number and a special character.");
         }
         
         
         
         //Username While Loop
+        
+        /* Farrel,J(2018)
+           Boolean is a data type that results in true or false only
+           We are chacking if the username is valid or not in order for the user to be registered 
+        */
         boolean usernameValid = false;
+        
+        /* Gravelle,R(2023)
+           While Loop allows the program to repeatedly ask the user for the input until it reaches the requiremenets/expectations
+           While Loop will run if the username is not correct
+        */
         while(!usernameValid)
+            
+            //if username is incorrect then the user is forced to type their username until it is corrrect for a successfull login
         { username =(JOptionPane.showInputDialog(null,"Enter your username" ));
         
              if( ! lg.CheckUsername(username))
             {
-                JOptionPane.showMessageDialog(null,"Username is not correctly formatted, please ensure that your username contains an underscore and is no more than 5 characters in length.");
+                //System will dispaly this message everytime the username is incorrectly formatted by user
+                JOptionPane.showMessageDialog(null,"Username is not correctly formatted, please ensure that your username contains"
+                 + " an underscore and is no more than 5 characters in length.");
             }  
             else
             {
@@ -66,11 +100,14 @@ public class POEPart1 {
         
         //Password WhileLoop
         boolean passwordValid = false;
-        while(!passwordValid)
+        while(!passwordValid) //if the password is wrong the while loop will continue
+            //user is prompted for password input until it is correct to continue
         { password =(JOptionPane.showInputDialog(null,"Enter your password" ));
              if( ! lg.CheckPasswordComplexity(password))
             {
-                JOptionPane.showMessageDialog(null,"Password is not correctly formatted, please ensure that the passsword contains at least 8 characters, a capital letter, a number and a special character");
+                //if password is wrong then system will output this message
+                JOptionPane.showMessageDialog(null,"Password is not correctly formatted, please ensure that the passsword contains"
+                 + " at least 8 characters, a capital letter, a number and a special character");
             }  
             else
             {
@@ -78,13 +115,12 @@ public class POEPart1 {
             }
         }
         
-        
 
-       
-        lg.registerUser();
+       // if password and username are the same and correctly given the the user is successfully registered
+        lg.registerUser(); //calling registerUser() method from Login class
         
         
-        
+        //Checks if the userrname and password are correct so user can be successfully logged in and display that message
         if(lg.LoginUser(username, password) == true) {
             JOptionPane.showMessageDialog(null,"You have been successfully logged in");
         } else {
@@ -93,7 +129,8 @@ public class POEPart1 {
             
         
        
-        
+        //if the user has successfully logged in then the Welcome message will be displayed along with their first and last name
+        //else they have to reguster and login again to recieve a welcome message
         if(lg.returnLoginStatus().equals("Successfull login")) {
            JOptionPane.showMessageDialog(null, "Welcome " + firstname + " "+ lastname + 
         " it is great to see you again.");
@@ -104,10 +141,10 @@ public class POEPart1 {
         
         
     }
-}
 
 
-/*
+ /*References
+
 Anonymous. 2021. Boolean Keyword in Java. (Version 1.0-SNAPSHOT) [Source code]. https://www.javatpoint.com/boolean-keyword-in-java (Accessed 1 April 2024)
 Anonymous. 2023. Java Stringformat() Method with Examples. (Version 1.0-SNAPSHOT) [Source code]. https://www.geeksforgeeks.org/java-string-format-method-with-examples/ (Accessed 1 April 2024)
 Baeldung  Michal,A. Java Check a String for Lowercase/Uppercase Letter, Special Character and Digit. (Version 1.0-SNAPSHOT) [Source code]. https://www.baeldung.com/java-lowercase-uppercase-special-character-digit-regex (Accessed 11 April 2024)
@@ -122,3 +159,4 @@ Ramgulia, G. 2023. Java.equals() Method: A Detailed Tutorial. (Version 1.0-SNAPS
 Shrivasta, A. 2023. Unit Testing in Java with JUnit. (Version 1.0-SNAPSHOT) [Source code]. https://www.browserstack.com/guide/unit-testing-java (Accessed 11 April 2024)
 */
 
+}
